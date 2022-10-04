f44d51822914d9148f3c076acea0eed84358cea0
youbaidjis <youba.etud@gmail.com>
2022-10-04 13:53:01 -0700

    HouseIndex

--- client/src/components/HousesIndex.js
+++ client/src/components/HousesIndex.js
@@ -5,16 +5,13 @@ import Row from 'react-bootstrap/Row';
 
 function HousesIndex(){
     const [houses, setHouses] = useState([]);
-
-    // const token = localStorage.getItem("jwt");
-    let token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.ex_5TEl4VoT38KLkMIZglZSbXpdUB1APbUgkgh9iZLA"
    
      useEffect(()=>{
         fetch("/houses",{
             method: "GET",
             headers:{
                 "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
+                 "Authorization": `Bearer ${localStorage.getItem("token")}`
             },
         }).then(res=>res.json())
            .then(result=> setHouses(result))
@@ -23,13 +20,13 @@ function HousesIndex(){
 
     return (
         <CardGroup>
-           <Row xs={2} md={3} className="g-3"> 
-        {
-        houses.map((house, index)=>{
-             return   <HouseCard  key={index} house={house}/>
-        })
-        }
-            </Row>
+        <Row xs={2} md={3} className="g-3"> 
+         {
+         houses.map((house, index)=>{
+              return   <HouseCard  key={index} house={house}/>
+         })
+         }
+          </Row>
         </CardGroup>
     )
 }
