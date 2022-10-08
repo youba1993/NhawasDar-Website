9d6c9131ae3edd9f09077bdbc895e5ef35eb0643
youbaidjis <youba.etud@gmail.com>
2022-10-07 21:49:53 -0700

    rebuild navbar component

--- client/src/components/home/NavBar.js
+++ client/src/components/home/NavBar.js
@@ -8,13 +8,16 @@ import Button from 'react-bootstrap/Button';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from '../redux/actions/LoginAction';
+import Nav from 'react-bootstrap/Nav';
+
 
 function NavBar() {
-  let currentUser = useSelector((state)=> {return state});
+  let currentUser = useSelector((state) => { return state });
   let dispatch = useDispatch();
   let navigate = useNavigate();
+  console.log(currentUser)
 
-  const  handleLogout = (e)=>{
+  const handleLogout = (e) => {
     localStorage.removeItem("token")
     dispatch(logout())
     navigate(0);
@@ -31,32 +34,54 @@ function NavBar() {
           <div className="vr" />
           <DropdownButton size="sm" id="dropdown-basic-button" variant="outline-danger" title="Sign Up">
             <Dropdown.Item as={Link} to="/userSignup">As Renter</Dropdown.Item>
-            <Dropdown.Item as={Link} to="/landlordSignup">As Landlord</Dropdown.Item> 
+            <Dropdown.Item as={Link} to="/landlordSignup">As Landlord</Dropdown.Item>
           </DropdownButton>
         </Stack>
       )
-    }else{
-      return <Button size="sm" variant="outline-danger" title="Log out" type="radio" onClick={(e)=>handleLogout (e)}>Log out </Button>
+    } else {
+
+      return <Button size="sm" variant="outline-danger" title="Log out" type="radio" onClick={(e) => handleLogout(e)}>Log out </Button>
+    }
+  }
+
+  const userButtons = () => {
+    if (currentUser.user === undefined) {
+      return (
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to='landlord/addListing'>add Listing</Nav.Link>
+          <Nav.Link as={Link} to='/landlord/houses'>My Listing</Nav.Link>
+        </Nav>
+      )
+    }
+    else {
+      if (currentUser.user.id !== 0) {
+        return (
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to='/renter/contract'>Contracts</Nav.Link>
+          </Nav>
+        )
+      } else return null
     }
   }
 
 
 
-//--------------------------------------------------------------------------------------------------
+  //--------------------------------------------------------------------------------------------------
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" >
       <Container fluid>
-        <Navbar.Brand  as={Link} to="/" >NhawasDar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Brand as={Link} to="/" >NhawasDar</Navbar.Brand>
+        {userButtons()}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="navbarScroll" className=" justify-content-end">
 
-        {authButtons()}
+          {authButtons()}
 
-      </Navbar.Collapse>
-    </Container>
+        </Navbar.Collapse>
+      </Container>
     </Navbar >
   );
 
 }
 
-export default NavBar ;
+export default NavBar;
