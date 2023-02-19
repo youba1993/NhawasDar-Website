6ee0881b8b16faf421ef9dac0ecf0ac8ce13038d
Youba IDJIS <100829124+youba1993@users.noreply.github.com>
2023-02-19 06:57:34 +0000

    --legacy-peer-deps

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # Build commands for front end to create the production build
 rm -rf public
-npm install --prefix client && npm run build --prefix client
+npm install --prefix --legacy-peer-deps client && npm run build --prefix --legacy-peer-deps client
 cp -a client/build/. public/
 
 # Build commands for back end
