86a6f1feeb2eeffc7c865bc180c78ae053fa60c9
Youba IDJIS <100829124+youba1993@users.noreply.github.com>
2023-02-19 08:47:38 +0000

    b

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,6 +9,5 @@ cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
-sudo service postgresql restart
 bundle exec rake db:migrate 
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
